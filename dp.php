145d67d0242082f30320c1ba82e38c23e6d93440
ppk2k <seell.vn@gmail.com>
2024-11-06 23:40:52 +0700

    Update dp.php

--- dp.php
+++ dp.php
@@ -1,1 +1,12 @@
+<?php
+$host = 'hostname';  // Misalnya 'localhost' atau alamat server eksternal Anda
+$username = 'db_user'; // Username database
+$password = 'db_password'; // Password database
+$database = 'db_name'; // Nama database yang digunakan
 
+$conn = new mysqli($host, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
